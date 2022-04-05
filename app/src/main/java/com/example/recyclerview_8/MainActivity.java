a9c440605f614e282208c4c0d31e7cb37ed6405e
Aziz Xushvaqtov <95211018+aziz0675@users.noreply.github.com>
2022-04-05 23:21:17 +0500

    RecyclerView_8 RecyclerView as Horizontal

---
+++ app/src/main/java/com/example/recyclerview_8/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.recyclerview_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.example.recyclerview_8.adapter.CustomAdapter;
+import com.example.recyclerview_8.model.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+
+    }
+    private void initViews() {
+        context = this;
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        refreshAdapter(prepareMemberList());
+    }
+
+    private void refreshAdapter(List<Member> members) {
+        CustomAdapter adapter = new CustomAdapter(context,members);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<Member> prepareMemberList() {
+        List<Member> members = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            members.add(new Member("Botirov" + i,"Abdulla " + i));
+        }
+        return members;
+    }
+}
